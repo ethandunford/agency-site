57cf7d4a8f7ec8429cead8624d659a5744573b30
ethan dunford <ethan.dunford@inchora.com>
2023-01-13 13:29:08 +0000

    home page form completed

---
+++ src/app/form.js
@@ -0,0 +1,17 @@
+import zenScroll from "zenscroll";
+
+export const options = ["Select", "Seo", "Brand Awareness", "Graphic Designs", "Web Development", "Other"]
+
+export const scrollToError = () => {
+  const errors = document.getElementsByClassName("error-msg")
+  if (errors.length > 0) {
+    zenScroll.center(document.getElementsByClassName("error-msg")[0])
+  }
+}
+
+export const successMessage = (success) => {
+  if (success) {
+    return <p className="has-text-black mt-3">we have received and a member of the team will get back shortly.</p>
+  }
+  return ""
+}
--- src/components/hero.js
+++ src/components/hero.js
@@ -1,10 +1,64 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect } from 'react';
+import { scrollToError, successMessage, options } from '../app/form';
 import TextInput from '../components/inputs/input'
+import SelectBox from '../components/inputs/select'
+import AppButton from '../components/button'
+import Error from '../components/error'
 import '../styles/hero.scss'
 
 const Hero = () => {
 
-  const [form, updateForm] = useState({ name: "", email: "", enquiry: "" })
+  const [loading, setLoading] = useState(false)
+  const [disabled, setDisabled] = useState(false)
+  const [success, setSuccess] = useState(false)
+  const [form, updateForm] = useState({ name: "", email: "", enquiry: "", })
+  const [errors, updateErrors] = useState({})
+
+  useEffect(() => {
+    scrollToError()
+  }, [errors])
+
+  const handleFormChange = (event) => {
+    if (event.target.name) {
+      const _form = Object.assign({}, form)
+      _form[event.target.name] = event.target.value
+      updateForm(_form)
+    }
+  }
+
+  const submit = (event) => {
+    event.preventDefault()
+    updateErrors({})
+    setLoading(true)
+    setDisabled(true)
+    setSuccess(false)
+    const _errors = {}
+
+    Object.keys(form).forEach((el) => {
+      if (form[el] === "") {
+        _errors[el] = "Required field"
+      }
+    })
+
+    if (form["enquiry"] === "Select") {
+      _errors["enquiry"] = "Required field"
+    }
+
+    setTimeout(() => {
+      if (Object.keys(_errors).length > 0) {
+        updateErrors(_errors)
+        setSuccess(false)
+        setLoading(false)
+        setDisabled(false)
+
+      } else {
+        setSuccess(true)
+        setLoading(false)
+        setDisabled(false)
+        updateForm({ name: "", email: "", enquiry: "", message: "" })
+      }
+    }, 2000)
+  }
 
   return <section className="hero is-large home-hero">
     <div className="container">
@@ -20,22 +74,26 @@ const Hero = () => {
             <div className="field">
               <label className="label">Name</label>
               <div className="control">
-                <TextInput placeholder="Name" value={form.name} />
+                <TextInput name="name" placeholder="Name" value={form.name} disabled={disabled} handleChange={handleFormChange} />
+                <Error msg={errors.name} />
               </div>
             </div>
             <div className="field">
               <label className="label">Email</label>
               <div className="control">
-                <TextInput placeholder="Email" value={form.email} />
+                <TextInput name="email" placeholder="Email" value={form.email} disabled={disabled} handleChange={handleFormChange} />
+                <Error msg={errors.email} />
               </div>
             </div>
             <div className="field">
               <label className="label">Enquiry</label>
               <div className="control">
-                <TextInput placeholder="Enquiry" value={form.name} />
+                <SelectBox name="enquiry" value={form.enquiry} options={options} disabled={disabled} handleChange={handleFormChange} />
+                <Error msg={errors.enquiry} />
               </div>
             </div>
-            <button className="button is-primary">Contact Us</button>
+            <AppButton btnText="Contact Us" loading={loading} disabled={disabled} handClick={submit} />
+            {successMessage(success)}
           </form>
         </div>
       </div>
